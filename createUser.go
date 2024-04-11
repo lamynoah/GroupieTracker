efe4a6e2031c1490118c002d41c79f1dcc9d755e
lamyn <noah.lamy@ynov.com>
2024-04-11 16:27:07 +0200

    create and add data in bdd users and login system

--- createUser.go
+++ createUser.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    _ "github.com/mattn/go-sqlite3"
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func createUserTable() {
-    
-    db, err := sql.Open("sqlite3", "./BDD/Users.db")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    defer db.Close()
-    createTableQuery := `
+func createUserTable() error {
+
+	db, err := sql.Open("sqlite3", "./BDD/Users.db")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	createTableQuery := `
         CREATE TABLE IF NOT EXISTS Users (
             UserID INTEGER PRIMARY KEY AUTOINCREMENT,
             Username TEXT NOT NULL UNIQUE,
@@ -22,9 +22,32 @@ func createUserTable() {
             Email TEXT NOT NULL UNIQUE
         );
     `
-    _, err = db.Exec(createTableQuery)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	_, err = db.Exec(createTableQuery)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func insertUser(username, email, password string) error {
+	db, err := sql.Open("sqlite3", "./BDD/Users.db")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	insertQuery := `INSERT INTO USERS (Username, Password, Email) Values (?,?,?)`
+	_, err = db.Exec(insertQuery, username, password, email)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func hashPassword(password string) (string, error) {
+	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hasedPassword), nil
 }
--- server.go
+++ server.go
@@ -2,12 +2,44 @@ package main
 
 import (
 	"net/http"
+	"text/template"
 )
 
-
- // local server
 func main() {
 	createUserTable()
+	http.HandleFunc("/signIn", func(w http.ResponseWriter, r *http.Request) {
+		temp, _ := template.ParseFiles("./pages/signIn.html", "./template/signIn.html")
+		username := r.FormValue("username")
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+		hasedPassword, err := hashPassword(password)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		err = insertUser(username, email, hasedPassword)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		temp.Execute(w, nil)
+	})
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		usernameOrEmail := r.FormValue("usernameOrEmail")
+		password := r.FormValue("password")
+		hasshedPassword, err := hashPassword(password)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+
+
+
+	})
+
+
+
 	fs := http.FileServer(http.Dir("."))
 	http.Handle("/", fs)
 	http.ListenAndServe(":8080", nil)
