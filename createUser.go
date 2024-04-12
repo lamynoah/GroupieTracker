d041b2e4d3d5e9d73cbfeb2d8eeb5ad5673c2426
lamyn <noah.lamy@ynov.com>
2024-04-12 15:09:55 +0200

    logIn and signIn

--- createUser.go
+++ createUser.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func createUserTable() error {
+func CreateUserTable() error {
 
 	db, err := sql.Open("sqlite3", "./BDD/Users.db")
 	if err != nil {
@@ -29,7 +30,7 @@ func createUserTable() error {
 	return nil
 }
 
-func insertUser(username, email, password string) error {
+func InsertUser(username, email, password string) error {
 	db, err := sql.Open("sqlite3", "./BDD/Users.db")
 	if err != nil {
 		return err
@@ -44,10 +45,114 @@ func insertUser(username, email, password string) error {
 	return nil
 }
 
-func hashPassword(password string) (string, error) {
+func HashPassword(password string) (string, error) {
 	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		return "", err
 	}
 	return string(hasedPassword), nil
 }
+
+func QueryNameUsers(db *sql.DB) ([]string, error) {
+	rows, err := db.Query("SELECT Username FROM Users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var usernames []string
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			return nil, err
+		}
+		usernames = append(usernames, name)
+	}
+	return usernames, nil
+}
+
+func QueryEmailUsers(db *sql.DB) ([]string, error) {
+	rows, err := db.Query("SELECT Email FROM Users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var Emails []string
+	for rows.Next() {
+		var email string
+		if err := rows.Scan(&email); err != nil {
+			return nil, err
+		}
+		Emails = append(Emails, email)
+	}
+	return Emails, nil
+}
+
+func QueryPasswordUsers(db *sql.DB) ([]string, error) {
+	rows, err := db.Query("SELECT Password FROM Users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var Passwords []string
+	for rows.Next() {
+		var password string
+		if err := rows.Scan(&password); err != nil {
+			return nil, err
+		}
+		Passwords = append(Passwords, password)
+	}
+	return Passwords, nil
+}
+
+type User struct {
+	email    string
+	password string
+}
+
+func GetAllUsersDetails(db *sql.DB) (map[string]User, error) {
+	detailsUsers := make(map[string]User)
+	usernames, err := QueryNameUsers(db)
+	if err != nil {
+		return nil, err
+	}
+	emails, err := QueryEmailUsers(db)
+	if err != nil {
+		return nil, err
+	}
+	passwords, err := QueryPasswordUsers(db)
+	if err != nil {
+		return nil, err
+	}
+	for i, username := range usernames {
+		detailsUsers[username] = User{
+			emails[i],
+			passwords[i],
+		}
+	}
+	return detailsUsers, nil
+}
+
+func IsMatch(usernameOrEmail, password string, db *sql.DB) (bool, error) {
+	users, err := GetAllUsersDetails(db)
+	if err != nil {
+		return false, err
+	}
+	for userName, userValue := range users {
+		fmt.Println(userName, userValue)
+		if usernameOrEmail == userName || usernameOrEmail == userValue.email {
+			if password == userValue.password {
+				return true, nil
+			} else {
+				return false, fmt.Errorf("Incorrect password: " + password + " != " + userValue.password)
+			}
+		}
+	}
+	return false, nil
+}
+
+// username or email => parcourir username = true
+
+// passwor = 1235 => hash
+// bdd.password => hash.password = 1235 et que user or email = same password [index]
--- server.go
+++ server.go
@@ -1,44 +1,63 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"text/template"
 )
 
 func main() {
-	createUserTable()
+	CreateUserTable()
+	http.HandleFunc("/selectGame", func(w http.ResponseWriter, r *http.Request) {
+		temp, _ := template.ParseFiles("./pages/selectGame.html")
+		temp.Execute(w, nil)
+	})
 	http.HandleFunc("/signIn", func(w http.ResponseWriter, r *http.Request) {
 		temp, _ := template.ParseFiles("./pages/signIn.html", "./template/signIn.html")
+		temp.Execute(w, nil)
+	})
+	http.HandleFunc("/createUser", func(w http.ResponseWriter, r *http.Request) {
 		username := r.FormValue("username")
 		email := r.FormValue("email")
 		password := r.FormValue("password")
-		hasedPassword, err := hashPassword(password)
+		hasedPassword, err := HashPassword(password)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		err = insertUser(username, email, hasedPassword)
+		err = InsertUser(username, email, hasedPassword)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+		http.Redirect(w, r, "/selectGame", http.StatusFound)
+	})
 
+	http.HandleFunc("/logIn", func(w http.ResponseWriter, r *http.Request) {
+		temp, _ := template.ParseFiles("./pages/logIn.html")
 		temp.Execute(w, nil)
 	})
-	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/loginUser", func(w http.ResponseWriter, r *http.Request) {
+		db, err := sql.Open("sqlite3", "./BDD/Users.db")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer db.Close()
 		usernameOrEmail := r.FormValue("usernameOrEmail")
 		password := r.FormValue("password")
-		hasshedPassword, err := hashPassword(password)
+		hashedPassword, err := HashPassword(password)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-
-
-
+		result, err := IsMatch(usernameOrEmail, hashedPassword, db)
+		if !result && err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w, r, "/selectGame", http.StatusFound)
 
 	})
-
-
 
 	fs := http.FileServer(http.Dir("."))
 	http.Handle("/", fs)
