25e1f64d95ebea7b06223d8cc601b9780a8758c1
lamyn <noah.lamy@ynov.com>
2024-05-13 23:57:14 +0200

    blindtest

--- api/requestSong.go
+++ api/requestSong.go
@@ -17,65 +17,50 @@ type Track struct {
 	} `json:"artists"`
 }
 
+type PlaylistData struct {
+	Tracks struct {
+		Items []struct {
+			Track struct {
+				ID string `json:"id"`
+			} `json:"track"`
+		} `json:"items"`
+	} `json:"tracks"`
+}
+
 func GetAccessToken() string {
-	accessToken := auth.Token()
-	return accessToken
+	return auth.Token()
 }
 
 func RequestSong() string {
 	accessToken := GetAccessToken()
 	playlistID := RequestPlaylist()
 	trackID := selectRandomTrack(playlistID)
-	url := fmt.Sprintf("https://api.spotify.com/v1/tracks/%s", trackID)
+	trackData := getTrackData(accessToken, trackID)
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Fatal("Error creating request:", err)
-	}
-
-	req.Header.Set("Authorization", "Bearer "+accessToken)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal("error sending request", err)
-	}
-	defer resp.Body.Close()
-
-	var trackData Track
-	if err := json.NewDecoder(resp.Body).Decode(&trackData); err != nil {
-		log.Fatal("Error decoding JSON", err)
-	}
-
-	fmt.Println("Track:", trackData.Name)
-	fmt.Println("Artist:")
-	for _, artist := range trackData.Artists {
-		fmt.Println("-", artist.Name)
-	}
+	printTrackInfo(trackData)
 
 	return trackID
 }
 
 func selectRandomTrack(playlistID string) string {
-	tracks := extractTrackID(playlistID)
+	tracks := extractTrackIDs(playlistID)
 
 	if len(tracks) == 0 {
-		log.Fatal("Playlist is empty")
+		log.Fatal("La playlist est vide")
 	}
 
 	rand.Seed(time.Now().UnixNano())
-
 	randomIndex := rand.Intn(len(tracks))
 	return tracks[randomIndex]
 }
 
-func extractTrackID(playlistID string) []string {
+func extractTrackIDs(playlistID string) []string {
 	accessToken := GetAccessToken()
 	url := fmt.Sprintf("https://api.spotify.com/v1/playlists/%s", playlistID)
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Fatal("Error creating request:", err)
+		log.Fatal("Erreur lors de la création de la requête:", err)
 	}
 
 	req.Header.Set("Authorization", "Bearer "+accessToken)
@@ -83,18 +68,52 @@ func extractTrackID(playlistID string) []string {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal("error sending request", err)
+		log.Fatal("Erreur lors de l'envoi de la requête", err)
 	}
 	defer resp.Body.Close()
 
+	var playlistData PlaylistData
 	if err := json.NewDecoder(resp.Body).Decode(&playlistData); err != nil {
-		log.Fatal("Error decoding JSON", err)
+		log.Fatal("Erreur lors du décodage du JSON", err)
 	}
 
-	var trackID []string
+	var trackIDs []string
 	for _, item := range playlistData.Tracks.Items {
-		trackID = append(trackID, item.Track.ID)
+		trackIDs = append(trackIDs, item.Track.ID)
 	}
 
-	return trackID
+	return trackIDs
+}
+
+func getTrackData(accessToken, trackID string) Track {
+	url := fmt.Sprintf("https://api.spotify.com/v1/tracks/%s", trackID)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("Erreur lors de la création de la requête:", err)
+	}
+
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Erreur lors de l'envoi de la requête", err)
+	}
+	defer resp.Body.Close()
+
+	var trackData Track
+	if err := json.NewDecoder(resp.Body).Decode(&trackData); err != nil {
+		log.Fatal("Erreur lors du décodage du JSON", err)
+	}
+
+	return trackData
+}
+
+func printTrackInfo(trackData Track) {
+	fmt.Println("Titre :", trackData.Name)
+	fmt.Println("Artistes :")
+	for _, artist := range trackData.Artists {
+		fmt.Println("-", artist.Name)
+	}
 }
